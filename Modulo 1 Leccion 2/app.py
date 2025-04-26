0d103d6b283d72365f910e42219ba8360ba4e5ed
Venus <160031273+venus8603@users.noreply.github.com>
2025-04-25 22:16:16 -0400

    Add files via upload

--- Modulo 1 Leccion 2/app.py
+++ Modulo 1 Leccion 2/app.py
@@ -2,41 +2,25 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Datos de usuarios en memoria
-usuarios = {"usuarios": []}
+todos = { "todos": ["Estudiar", "Comer", "Jugar"]}
 
 @app.route("/", methods=["GET"])
 def home():
-    return "API de gestión de usuarios"
+    return "Simple TODO API"
 
-@app.route("/info", methods=["GET"])
-def info():
-    return jsonify({
-        "mensaje": "Sistema de gestión de usuarios",
-        "version": "1.0",
-        "autor": "Tu Nombre"
-    })
+@app.route("/todos", methods=["GET"])
+def get_todos():
+    return jsonify(todos)
 
-@app.route("/usuarios", methods=["GET"])
-def get_usuarios():
-    return jsonify(usuarios)
-
-@app.route("/crear_usuario", methods=["POST"])
-def crear_usuario():
+@app.routte("/todos", methods=["POST"])
+def insert_todo():
     data = request.json
-
-    if not data or "nombre" not in data or "correo" not in data:
-        return jsonify({"error": "Datos incompletos: se requiere 'nombre' y 'correo'."}), 400
-
-    nuevo_usuario = {
-        "id": len(usuarios["usuarios"]) + 1,
-        "nombre": data["nombre"],
-        "correo": data["correo"]
-    }
+    if not data or "todo" not in data:
+        return jsonify({"error": "Datos incompl"}), 400
     
-    usuarios["usuarios"].append(nuevo_usuario)
-
-    return jsonify({"mensaje": "Usuario creado exitosamente.", "usuario": nuevo_usuario})
+    todos["todos"].insert(data['todo'])
+    
+    return jsonify(todos)
 
 if __name__ == "__main__":
     app.run(debug=True)
